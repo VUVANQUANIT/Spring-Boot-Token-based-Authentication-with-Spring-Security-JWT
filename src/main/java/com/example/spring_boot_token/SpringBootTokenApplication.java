85984ea09c3205c74644deb366f44b156b326a67
VUVANQUANIT <quancn27@gmail.com>
2025-06-27 17:19:30 +0700

    Initial commit

---
+++ src/main/java/com/example/spring_boot_token/SpringBootTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTokenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/spring_boot_token/models/ERole.java
@@ -0,0 +1,7 @@
+package com.example.spring_boot_token.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
